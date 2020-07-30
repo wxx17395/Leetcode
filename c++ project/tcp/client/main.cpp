3af16ddab070f0cca4f0126a23e048aa8032ab1f
MyUbuntu <397511627@qq.com>
2020-07-30 04:47:35 -0700

    tcp linux

--- c++ project/tcp/client/main.cpp
+++ c++ project/tcp/client/main.cpp
@@ -1,79 +1,66 @@
 #include <bits/stdc++.h>
-#include<winsock.h>
-#pragma comment(lib,"ws2_32.lib")
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+
 using namespace std;
-void initialization();
-int main() {
-    //定义长度变量
-    int send_len = 0;
-    int recv_len = 0;
-    //定义发送缓冲区和接受缓冲区
-    char send_buf[100];
-    char recv_buf[100];
-    //定义服务端套接字，接受请求套接字
-    SOCKET s_server;
-    //服务端地址客户端地址
-    SOCKADDR_IN server_addr;
-    initialization();
-    //填充服务端信息
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
-    server_addr.sin_port = htons(1234);
-    //创建套接字
-    s_server = socket(AF_INET, SOCK_STREAM, 0);
-    if (connect(s_server, (SOCKADDR *)&server_addr, sizeof(SOCKADDR)) == SOCKET_ERROR) {
-        cout << "服务器连接失败！" << endl;
-        WSACleanup();
-    }
-    else {
-        cout << "服务器连接成功！" << endl;
-    }
 
-    //发送,接收数据
-    while (1) {
-        cout << "请输入发送信息:";
-        cin >> send_buf;
-        send_len = send(s_server, send_buf, 100, 0);
-        if (send_len < 0) {
-            cout << "发送失败！" << endl;
-            break;
-        }
-        recv_len = recv(s_server, recv_buf, 100, 0);
-        if (recv_len < 0) {
-            cout << "接受失败！" << endl;
-            break;
-        }
-        else {
-            cout << "服务端信息:" << recv_buf << endl;
-        }
+#define SERV_PORT 9527
+#define buffersize 1024
+typedef int SOCKET;
 
-    }
-    //关闭套接字
-    closesocket(s_server);
-    //释放DLL资源
-    WSACleanup();
-    return 0;
+void sys_err(const char *err_str){
+	perror(err_str);
+	exit(1);
 }
-void initialization() {
-    //初始化套接字库
-    WORD w_req = MAKEWORD(2, 2);//版本号
-    WSADATA wsadata;
-    int err;
-    err = WSAStartup(w_req, &wsadata);
-    if (err != 0) {
-        cout << "初始化套接字库失败！" << endl;
-    }
-    else {
-        cout << "初始化套接字库成功！" << endl;
-    }
-    //检测版本号
-    if (LOBYTE(wsadata.wVersion) != 2 || HIBYTE(wsadata.wHighVersion) != 2) {
-        cout << "套接字库版本号不符！" << endl;
-        WSACleanup();
-    }
-    else {
-        cout << "套接字库版本正确！" << endl;
-    }
-    //填充服务端地址信息
 
+int main(){
+	int send_len = 0;
+	int recv_len = 0;
+	const int len = sizeof(sockaddr);
+
+	char send_buf[buffersize];
+	char recv_buf[buffersize];
+
+	SOCKET s_server;
+
+	sockaddr_in server_addr;
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(SERV_PORT);
+	inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr.s_addr);
+
+	s_server = socket(AF_INET, SOCK_STREAM, 0);
+	if (s_server == -1){
+		sys_err("socket error");
+	}
+	
+	if (connect(s_server, (sockaddr*) &server_addr, sizeof(sockaddr)) == -1){
+        sys_err("connect error");
+    }
+
+    cout << "connect succeed" << endl;
+	while (1){
+        cout << "Input send message:";
+		cin >> send_buf;
+		send_len = send(s_server, send_buf, sizeof(send_buf), 0);
+		if (send < 0){
+			cout << "send failed!" << endl;
+			break;
+		}
+		recv_len = recv(s_server, recv_buf, buffersize, 0);
+		if (recv_len < 0){
+			cout << "receive failed!" << endl;
+			break;
+		} else {
+			cout << "clien message:" << recv_buf << endl; 
+		}
+	}
+	
+	close(s_server);
+	return 0;
 }
--- c++ project/tcp/server/main.cpp
+++ c++ project/tcp/server/main.cpp
@@ -1,100 +1,77 @@
-#include <iostream>
-#include <winsock.h>
-#pragma comment(lib,"ws2_32.lib")
+#include <bits/stdc++.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+
 using namespace std;
 
-void initialization();
-int main() {
-	//定义长度变量
+#define SERV_PORT 9527
+#define buffersize 1024
+typedef int SOCKET;
+
+void sys_err(const char *err_str){
+	perror(err_str);
+	exit(1);
+}
+
+int main(){
 	int send_len = 0;
 	int recv_len = 0;
-	int len = 0;
-	//定义发送缓冲区和接受缓冲区
-	char send_buf[100];
-	char recv_buf[100];
-	//定义服务端套接字，接受请求套接字
+	const int len = sizeof(sockaddr);
+
+	char send_buf[buffersize];
+	char recv_buf[buffersize];
+
 	SOCKET s_server;
 	SOCKET s_accept;
-	//服务端地址客户端地址
-	SOCKADDR_IN server_addr;
-	SOCKADDR_IN accept_addr;
-	initialization();
-	//填充服务端信息
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
-	server_addr.sin_port = htons(1234);
-	//创建套接字
-	s_server = socket(AF_INET, SOCK_STREAM, 0);
-	if (bind(s_server, (SOCKADDR *)&server_addr, sizeof(SOCKADDR)) == SOCKET_ERROR) {
-		cout << "套接字绑定失败！" << endl;
-		WSACleanup();
-	}
-	else {
-		cout << "套接字绑定成功！" << endl;
-	}
-	//设置套接字为监听状态
-	if (listen(s_server, SOMAXCONN) < 0) {
-		cout << "设置监听状态失败！" << endl;
-		WSACleanup();
-	}
-	else {
-		cout << "设置监听状态成功！" << endl;
-	}
-	cout << "服务端正在监听连接，请稍候...." << endl;
-	//接受连接请求
-	len = sizeof(SOCKADDR);
-	s_accept = accept(s_server, (SOCKADDR *)&accept_addr, &len);
-	if (s_accept == SOCKET_ERROR) {
-		cout << "连接失败！" << endl;
-		WSACleanup();
-		return 0;
-	}
-	cout << "连接建立，准备接受数据" << endl;
-	//接收数据
-	while (1) {
-		recv_len = recv(s_accept, recv_buf, 100, 0);
-		if (recv_len < 0) {
-			cout << "接受失败！" << endl;
-			break;
-		}
-		else {
-			cout << "客户端信息:" << recv_buf << endl;
-		}
-		cout << "请输入回复信息:";
-		cin >> send_buf;
-		send_len = send(s_accept, send_buf, 100, 0);
-		if (send_len < 0) {
-			cout << "发送失败！" << endl;
-			break;
-		}
-	}
-	//关闭套接字
-	closesocket(s_server);
-	closesocket(s_accept);
-	//释放DLL资源
-	WSACleanup();
-	return 0;
-}
-void initialization() {
-	//初始化套接字库
-	WORD w_req = MAKEWORD(2, 2);//版本号
-	WSADATA wsadata;
-	int err;
-	err = WSAStartup(w_req, &wsadata);
-	if (err != 0) {
-		cout << "初始化套接字库失败！" << endl;
-	}
-	else {
-		cout << "初始化套接字库成功！" << endl;
-	}
-	//检测版本号
-	if (LOBYTE(wsadata.wVersion) != 2 || HIBYTE(wsadata.wHighVersion) != 2) {
-		cout << "套接字库版本号不符！" << endl;
-		WSACleanup();
-	}
-	else {
-		cout << "套接字库版本正确！" << endl;
-	}
-	//填充服务端地址信息
 
+	sockaddr_in server_addr;
+	sockaddr_in accept_addr;
+
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(SERV_PORT);
+	inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr.s_addr);
+
+	s_server = socket(AF_INET, SOCK_STREAM, 0);
+	if (s_server == -1){
+		sys_err("socket error");
+	}
+	
+	if (bind(s_server, (sockaddr*)&server_addr, len) == -1){
+		sys_err("bind error");
+	}
+
+	if (listen(s_server, 128) < 0){
+		sys_err("listen error");
+	}
+
+	s_accept = accept(s_server, (sockaddr*)&accept_addr, (socklen_t*)&len);
+	if (s_accept == -1){
+		sys_err("accept error");
+	}
+
+	while (1){
+		recv_len = recv(s_accept, recv_buf, buffersize, 0);
+		if (recv_len < 0){
+			cout << "receive failed!" << endl;
+			break;
+		} else {
+			cout << "clien message:" << recv_buf << endl; 
+		}
+		cout << "Input response message:";
+		cin >> send_buf;
+		send_len = send(s_accept, send_buf, sizeof(send_buf), 0);
+		if (send < 0){
+			cout << "send failed!" << endl;
+			break;
+		}
+	}
+	
+	close(s_server);
+	close(s_accept);
+	return 0;
 }
