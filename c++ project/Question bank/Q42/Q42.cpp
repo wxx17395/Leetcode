cd445a7a703af61b9f67fc4c0fef4ff1962a3685
wxx17395 <38486466+wxx17395@users.noreply.github.com>
2020-08-16 11:29:57 +0800

    20200816

--- c++ project/Question bank/Q42/Q42.cpp
+++ c++ project/Question bank/Q42/Q42.cpp
@@ -3,30 +3,22 @@
 using namespace std;
 
 class Solution {
-private:
-    int func(vector<int>& height, int l, int r){
-        int res= height[l] * (r - l - 1);
-        for (int i = l + 1; i < r; ++i){
-            res -= height[i];
-        }
-        return res;
-    }
 public:
     int trap(vector<int>& height) {
         stack<int> s;
         int res = 0;
         int tmp;
         for (int i = 0; i < height.size(); ++i){
-            tmp = 0;
-            while (!s.empty() && height[s.top()] <= height[i]){
-                tmp = max(tmp, func(height, s.top(), i));
-                if (height[s.top()] < height[i]){
-                    s.pop();
-                } else {
+            while (!s.empty() && height[s.top()] < height[i]){
+                int top = s.top();
+                s.pop();
+                if (s.empty()){
                     break;
                 }
+                int distance = i - s.top() - 1;
+                int bounded_height = min(height[i], height[s.top()]) - height[top];
+                res += distance * bounded_height;
             }
-            res += tmp;
             s.push(i);
         }
         return res;
