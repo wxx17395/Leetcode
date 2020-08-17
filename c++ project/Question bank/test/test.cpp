8e46522c5bfdd51c85caf678a45021dc3cbd5c46
MyUbuntu <397511627@qq.com>
2020-08-17 03:17:20 -0700

    20200817

--- c++ project/Question bank/test/test.cpp
+++ c++ project/Question bank/test/test.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main()
 {
-   int n;
-   cin >> n;
-   cin.sync();
-   string a, b;
-   getline(cin, a);
-   getline(cin, b);
-   cout << "a:" << a << ";b:" << b << ";" << endl; 
+   int nums[7] = {0,1,2,3,4,5,6};
+   int *num = nums + 2;
+   cout << num[0] << endl;
+   cout << num[-1] << endl;
+   cout << num[-2] << endl;
+   cout << num[-3] << endl;
+   return 0;
 }
